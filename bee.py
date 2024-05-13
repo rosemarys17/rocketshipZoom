be7a04badc43899dee10328f5cf2646f77ba164e
rosemarys17 <167535133+rosemarys17@users.noreply.github.com>
2024-05-13 08:45:28 -0400

    commit day 4

--- bee.py
+++ bee.py
@@ -5,13 +5,22 @@ class Bee:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("apple.png")
+        self.image = pygame.image.load("bee.png")
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.delta = .5
+        picture = pygame.image.load("bee.png")
+        picture = pygame.transform.scale(picture, (1280, 720))
 
-    def move(self, new_x, new_y):
-        self.x = new_x
-        self.y = new_y
+    def move_direction(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        if direction == "left":
+            self.x = self.x - self.delta
+        if direction == "up":
+            self.y = self.y - self.delta
+        if direction == "down":
+            self.y = self.y + self.delta
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from bee import Bee
 
 # set up pygame modules
 pygame.init()
@@ -9,11 +10,9 @@ pygame.display.set_caption("AP CSP Pygame!")
 # set up variables for the display
 size = (400, 300)
 screen = pygame.display.set_mode(size)
-
-name = "Mr. Das"
+bee = Bee(70,70)
 
 # render the text for later
-display_name = my_font.render(name, True, (255, 255, 255))
 display_title_screen1 = my_font.render("Use ASDW to move.", True, (255, 255, 255))
 display_title_screen2 = my_font.render("Pollinate each colorful flower without touching the black flowers! Don't let the bear steal your honey!",  True, (255, 255, 255))
 display_title_screen3 = my_font.render("Click anywhere on the screen to begin!", True, (255, 255, 255))
@@ -23,18 +22,32 @@ run = True
 
 # -------- Main Program Loop -----------
 while run:
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_RIGHT]:
+        bee.move_direction("right")
+    elif keys[pygame.K_LEFT]:
+        bee.move_direction("left")
+    elif keys[pygame.K_UP]:
+        bee.move_direction("up")
+    elif keys[pygame.K_DOWN]:
+        bee.move_direction("down")
+
     # --- Main event loop
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            title_screen = False
 
-    screen.fill((90, 17, 146))
+    screen.fill((90, 57, 146))
     if title_screen:
         screen.blit(display_title_screen1, (150, 200))
         screen.blit(display_title_screen2, (150, 230))
         screen.blit(display_title_screen3, (150, 260))
-
-    screen.blit(display_name, (0, 0))
+    else:
+        screen.blit(bee.image, bee.rect)
     pygame.display.update()
 
 # Once we have exited the main program loop we can stop the game engine:
