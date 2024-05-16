5501e47168697c4babd17775b6c41b23c8ca6400
rosemarys17 <167535133+rosemarys17@users.noreply.github.com>
2024-05-16 08:45:51 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from bee import Bee
 # set up pygame modules
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont('Arial', 15)
+my_font = pygame.font.SysFont('snapitc', 15)
 pygame.display.set_caption("AP CSP Pygame!")
 
+
+
 # set up variables for the display
-size = (400, 300)
+size = (700, 600)
 screen = pygame.display.set_mode(size)
-bee = Bee(70,70)
+bee = Bee(70, 70)
+background = pygame.image.load("background.png")
 
 # render the text for later
-display_title_screen1 = my_font.render("Use ASDW to move.", True, (255, 255, 255))
-display_title_screen2 = my_font.render("Pollinate each colorful flower without touching the black flowers! Don't let the bear steal your honey!",  True, (255, 255, 255))
-display_title_screen3 = my_font.render("Click anywhere on the screen to begin!", True, (255, 255, 255))
+display_title_screen1 = my_font.render("Use ASDW to move.", True, (0, 0, 0))
+display_title_screen2 = my_font.render("Pollinate each colorful flower without",  True, (0, 0, 0))
+display_title_screen3 = my_font.render("touching the black flowers! Don't let",  True, (0, 0, 0))
+display_title_screen4 = my_font.render(" the bear steal your honey!", True, (0, 0, 0))
+display_title_screen5 = my_font.render("Click the screen to begin!", True, (0, 0, 0))
 title_screen = True
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 run = True
@@ -41,12 +46,15 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             title_screen = False
 
-    screen.fill((90, 57, 146))
     if title_screen:
-        screen.blit(display_title_screen1, (150, 200))
-        screen.blit(display_title_screen2, (150, 230))
-        screen.blit(display_title_screen3, (150, 260))
+        screen.fill((255, 192, 0))
+        screen.blit(display_title_screen1, (125, 20))
+        screen.blit(display_title_screen2, (40, 100))
+        screen.blit(display_title_screen3, (45, 130))
+        screen.blit(display_title_screen4, (70, 160))
+        screen.blit(display_title_screen5, (45, 230))
     else:
+        screen.blit(background, (0, -130))
         screen.blit(bee.image, bee.rect)
     pygame.display.update()
 
